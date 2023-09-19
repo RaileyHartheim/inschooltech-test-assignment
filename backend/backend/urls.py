8942769df759869b96df3ccdb2034aa7f7255445
Yulia Kazhaeva <melancholia.raiven@gmail.com>
2023-09-19 21:51:12 +0500

    Fix versions for some packages in requirements.txt

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
-from drf_yasg2 import openapi
-from drf_yasg2.views import get_schema_view
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 
@@ -15,23 +15,6 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns = [
-   re_path(
-       r'swagger(?P<format>\.json|\.yaml)$',
-       schema_view.without_ui(cache_timeout=0),
-       name='schema-json'
-    ),
-   path(
-       'swagger/',
-       schema_view.with_ui('swagger', cache_timeout=0),
-       name='schema-swagger-ui'
-    ),
-   path(
-       'redoc/',
-       schema_view.with_ui('redoc', cache_timeout=0),
-       name='schema-redoc'
-    ),
-]
 
 urlpatterns = [
     re_path(
